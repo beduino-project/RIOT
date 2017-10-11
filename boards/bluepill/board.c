ec512b13f29f3d589610a1fc98b5f9acc080fb90
Sören Tempel <soeren+git@soeren-tempel.net>
2017-10-11 11:26:52 +0200

    bluepill: Initialize

---
+++ boards/bluepill/board.c
@@ -0,0 +1,22 @@
+/** TODO copyright comment *//
+
+/**
+ * @ingroup     boards_bluepill
+ * @{
+ *
+ * @file
+ * @brief       Board specific implementations for the bluepill board
+ *
+ * @author      TODO
+ *
+ * @}
+ */
+
+#include "cpu.h"
+#include "board.h"
+
+void board_init(void)
+{
+    /* TODO */
+    return;
+}
