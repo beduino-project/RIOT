e1dae76f20ed2816a05cb56257c950027f212551
Tristan Bruns <tristan+git@tristanbruns.de>
2018-08-03 15:47:44 +0200

    example for profiling using the CYCCNT

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -21,12 +21,35 @@
 
 #include <stdio.h>
 
+#include <stdint.h>
+#include "cpu_conf.h"
+#include "periph_conf.h"
+
 int main(void)
 {
+    // we need two 32 bit words for profiling
+    uint32_t profA, profB;
+
+    // enable the cycle counter (needed only once)
+    DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
+
+    // start profiling
+    profA = DWT->CYCCNT;
+
     puts("Hello World!");
 
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
+
+    // end profiling
+    profB = DWT->CYCCNT;
+
+    // interpret result
+    // (This assumes that the counter did not overflow, which is not
+    // generally a valid assumption, so take result with grain of salt)
+    profA = profB - profA;
+    profB = profA / (CLOCK_CORECLOCK / 1000000);
+    printf("PROF %lu ticks, %lu usecs\n", profA, profB);
 
     return 0;
 }
